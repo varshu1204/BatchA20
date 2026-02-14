bc9cb8b709d3934c05f54caa51f9d8569cf726d5
QSP <QSP@DESKTOP-K5OA249>
2026-02-14 12:16:43 +0530

    First time commiting

---
+++ src/test/java/Prac/Demo1Test.java
@@ -0,0 +1,17 @@
+package Prac;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	
+	@Test(groups = "smoke")
+	public void cars()
+	{
+		System.out.println("Benz");
+	}
+	
+	
+	
+	
+}
---
+++ src/test/java/Prac/DemoTest.java
@@ -0,0 +1,14 @@
+package Prac;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	
+	
+	@Test
+	public void bikes()
+	{
+		System.out.println("RE");
+	}
+}
